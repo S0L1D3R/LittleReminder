bdcce1d8a09e3ca87db9a2d638dc29cb24b7e3db
Admin <Admin@Komputer-Win>
2017-03-31 16:45:34 +0200

    Test3

--- src/pl/interia/rym/maciej/reminder/Main.java
+++ src/pl/interia/rym/maciej/reminder/Main.java
@@ -31,7 +31,7 @@ public class Main {
 	public static Main reminder;
 	
 	public static void main(String[] args) {
-		//Config.loadConfigs();dsadasdasdsad
+		//Config.loadConfigs();sssssssssssssssssssssssssssssssss
 		reminder = new Main();
 		reminder.listeners = new Listeners();
 		
